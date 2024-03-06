301214dc1d05d0b9b6b4052e8f251c1736f63168
Ivan Meirelles <hackermeirelles+github@gmail.com>
2024-03-06 00:11:30 -0300

    update

--- IMC.py
+++ IMC.py
@@ -1,28 +1,40 @@
 def calcula_imc(p, a):
+    if p <= 0 or a <= 0:
+        return None # Retorna None se peso ou altura não forem válidos
     imc = (p / (a**2))
-    print(f'Seu IMC é {imc:.2f}')
-    return IMC
+    return imc
 
 
 print('Ola! Bem vindo ao programa IMC')
+
 nome = input('Me diga seu nome: ')
 
-altura = float(input(f'Agora {nome} me diga sua altura :'))
-peso = float(input(f'Agora {nome} me diga seu peso :'))
+
+try:
+    altura = float(input(f'Agora {nome} me diga sua altura (em metros): '))
+    peso = float(input(f'Agora {nome} me diga seu peso (em quilogramas): '))
+except ValueError:
+    print('Por favor, insira valores válidos para altura e peso.')
+    exit()
 
 IMC = calcula_imc(peso, altura)
 
-if IMC < 17:
-    print(f'{nome}, seu IMC está MUITO ABAIXO do peso.')
-elif IMC < 18.5:
-    print(f'{nome}, seu IMC está ABAIXO do peso.')
-elif IMC < 25:
-    print(f'{nome}, seu IMC está NORMAL para o peso.')
-elif IMC < 30:
-    print(f'{nome}, seu IMC esta ACIMA do peso.')
-elif IMC < 35:
-    print(f'{nome}, seu IMC esta OBESIDADE GRAU I.')
-elif IMC < 40:
-    print(f'{nome}, seu IMC esta MOBESIDADE GRAU II.')
+if IMC is None:
+    print('Valores inválidos para altura ou peso.')
 else:
-    print(f'Seu estado é de Obesidade Grau III (mórbida)\nProcure um médico ou nutricionista.')
+    print(f'Seu IMC é {IMC:.2f}')
+
+    if IMC < 17:
+        print(f'{nome}, seu IMC está MUITO ABAIXO do peso.')
+    elif IMC < 18.5:
+        print(f'{nome}, seu IMC está ABAIXO do peso.')
+    elif IMC < 25:
+        print(f'{nome}, seu IMC está NORMAL para o peso.')
+    elif IMC < 30:
+        print(f'{nome}, seu IMC esta ACIMA do peso.')
+    elif IMC < 35:
+        print(f'{nome}, seu IMC esta OBESIDADE GRAU I.')
+    elif IMC < 40:
+        print(f'{nome}, seu IMC esta MOBESIDADE GRAU II.')
+    else:
+        print(f'Seu estado é de Obesidade Grau III (mórbida)\nProcure um médico ou nutricionista.')
